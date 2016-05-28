8d8eaa4a3a9e00aa05d51a238b0d27dd02654cb6
Sripathi Pai <sripathi.pai@skyscanner.net>
2016-05-28 21:03:51 +0100

    Bumped the version to 0.0.1

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='gousto_model',
     packages=find_packages(),
-    version='0.0.0.3',
+    version='0.0.1',
     install_requires=['pscore>=0.0.0.17']
 )
